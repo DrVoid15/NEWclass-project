04402b25f1dcfec63c799eaae5400cb97c6371be
DrVoid15 <skyfly1000@yahoo.com>
2025-07-14 12:37:45 -0700

    addproject

---
+++ My project (1)/Assets/Scripts/Pickups.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickups : MonoBehaviour
+{
+    //reference to player
+    public PlayerController Bean;
+    // Start is called before the first frame updatedd
+    void Start()
+    {
+        // grab fer to player 
+        Bean = GameObject.Find("Bean").GetComponent<PlayerController>();
+
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // if the player collides  with coin, increase points and destroy
+        if (other.name == "Bean")
+        {
+            Bean.coincount++;
+            Destroy(this.gameObject);
+
+        }
+    }
+}
